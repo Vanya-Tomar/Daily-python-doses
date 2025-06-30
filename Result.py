b97b6a65ee8abebf034c92941fc05338a32afdcc
Vanya-Tomar <vanya.tomar@gmail.com>
2025-06-30 23:38:37 +0530

    Update Result.py

--- Result.py
+++ Result.py
@@ -6,4 +6,8 @@ science=int(input("enter the number obtained in science subject:  "))
 maths=int(input("enter the number obtained in maths subject:   "))
 social science =int(input("enter the number obtained in social science :  "))
 list1=[]
-
+list1.append("english")
+list1.append("hindi")
+list1.append("science ")
+list1.append("maths")
+list1.append("social science")
